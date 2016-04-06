bebef213bd4a54b2dcd42ec83637a675d71cd179
Knyazev Sergey <yaknyazsergei@gmail.com>
2016-04-06 22:44:16 +0300

    Добвален калькулятор lab3.2

---
+++ lab3/calculator/calculator/Calculator.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Calculator.h"
+
+
+CCalculator::CCalculator()
+{
+}
+
+
+CCalculator::~CCalculator()
+{
+}
---
+++ lab3/calculator/calculator/main.cpp
@@ -0,0 +1,14 @@
+// calculator.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "Calculator.h"
+
+int main()
+{
+	CCalculator calculator;
+
+	calculator.add();
+    return 0;
+}
+
